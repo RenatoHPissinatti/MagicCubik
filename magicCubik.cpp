d26da411ac0153aaf6d98542ab2aef0c6567e787
RenatoHPissinatti <pissinatti.renato@outlook.com>
2025-08-19 22:45:59 -0300

    Adicionado instruções de movimentação

--- magicCubik.cpp
+++ magicCubik.cpp
@@ -32,8 +32,8 @@ class magicCubik2x2x2 {
             }
         }
     // Manipulação do cubo
-    void U (string up[2][2], string right[2][2], string left[2][2], string front[2][2], string back[2][2]) {
-    string temp[2][2];
+    void moveU () {
+    char temp[2][2];
     temp[0][0] = front[0][0];
     temp[0][1] = front[0][1];
     temp[1][0] = front[1][0];
@@ -51,15 +51,15 @@ class magicCubik2x2x2 {
     left[0][0] = temp[0][0];
     left[0][1] = temp[0][1];
 
-    string tempUp = up[0][0];
+    char tempUp = up[0][0];
     up[0][0] = up[1][0];
     up[1][0] = up[1][1];
     up[1][1] = up[0][1];
     up[0][1] = tempUp;
 
     }
-    void D (string down[2][2], string right[2][2], string left[2][2], string front[2][2], string back[2][2]) {
-        string temp[2][2];
+    void moveD () {
+        char temp[2][2];
         temp[0][0] = front[0][0];
         temp[0][1] = front[0][1];
         temp[1][0] = front[1][0];
@@ -77,15 +77,15 @@ class magicCubik2x2x2 {
         left[1][0] = temp[0][0];
         left[1][1] = temp[0][1];
 
-        string tempDown = down[0][0];
+        char tempDown = down[0][0];
         down[0][0] = down[0][1];
         down[0][1] = down[1][1];
         down[1][1] = down[1][0];
         down[1][0] = tempDown;
     }
 
-    void R (string up[2][2], string down[2][2], string right[2][2], string front[2][2], string back[2][2]) {
-        string temp[2][2];
+    void moveR () {
+        char temp[2][2];
         temp[0][0] = front[0][0];
         temp[0][1] = front[0][1];
         temp[1][0] = front[1][0];
@@ -103,7 +103,7 @@ class magicCubik2x2x2 {
         up[1][1] = temp[1][1];
         up[0][1] = temp[0][1];
 
-        string tempRight = right[0][0];
+        char tempRight = right[0][0];
 
         right[0][0] = right[1][0];
         right[1][0] = right[1][1];
@@ -111,8 +111,8 @@ class magicCubik2x2x2 {
         right[0][1] = tempRight;
     }
 
-    void L (string up[2][2], string down[2][2], string left[2][2], string front[2][2], string back[2][2]) {
-        string temp[2][2];
+    void moveL () {
+        char temp[2][2];
         temp[0][0] = front[0][0];
         temp[0][1] = front[0][1];
         temp[1][0] = front[1][0];
@@ -130,15 +130,15 @@ class magicCubik2x2x2 {
         up[0][0] = temp[0][0];
         up[1][0] = temp[1][0];
 
-        string tempLeft = left[0][0];
+        char tempLeft = left[0][0];
         left[0][0] = left[0][1];
         left[0][1] = left[1][1];
         left[1][1] = left[1][0];
         left[1][0] = tempLeft;
     }
 
-    void F (string up[2][2], string down[2][2], string left[2][2], string right[2][2], string front[2][2]) {
-        string temp[2][2];
+    void moveF () {
+        char temp[2][2];
         temp[0][0] = up[0][0];
         temp[0][1] = up[0][1];
         temp[1][0] = up[1][0];
@@ -156,15 +156,15 @@ class magicCubik2x2x2 {
         right[0][0] = temp[1][0];
         right[1][0] = temp[1][1];
 
-        string tempFront = front[0][0];
+        char tempFront = front[0][0];
         front[0][0] = front[1][0];
         front[1][0] = front[1][1];
         front[1][1] = front[0][1];
         front[0][1] = tempFront;
     }
 
-    void B (string up[2][2], string down[2][2], string left[2][2], string right[2][2], string back[2][2]) {
-        string temp[2][2];
+    void moveB () {
+        char temp[2][2];
         temp[0][0] = up[0][0];
         temp[0][1] = up[0][1];
         temp[1][0] = up[1][0];
@@ -182,7 +182,7 @@ class magicCubik2x2x2 {
         right[0][1] = temp[0][0];
         right[1][1] = temp[0][1];
 
-        string tempBack = back[0][0];
+        char tempBack = back[0][0];
         back[0][0] = back[0][1];
         back[0][1] = back[1][1];
         back[1][1] = back[1][0];
@@ -190,21 +190,26 @@ class magicCubik2x2x2 {
     }
 
     // Representação do cubo em 2D
-    //         [Left]
-    //  [Down] [Front] [Up]
-    //         [Right]
-    //         [Back]
+    //         [Up]
+    //[Left] [Front] [Right] [Back]
+    //        [Down]]
 
     void printCubik() {
-        cout << "\t" << "    "; cout << left[0][0] << " " << left[0][1] << endl;
-        cout << "\t" << "    "; cout << left[1][0] << " " << left[1][1] << endl;
-        cout << "\t" << down[0][0]<< " " << down[0][1] << " " << front[0][0]<<  " " << front[0][1] << " " << up[0][0] << " " << up[0][1] << endl;
-        cout << "\t" << down[1][0]<< " " << down[1][1] << " " << front[1][0]<<  " " << front[1][1] << " " << up[1][0] << " " << up[1][1] << endl;
-        cout << "\t" << "    "; cout << right[0][0] << " " << right[0][1] << endl;
-        cout << "\t" << "    "; cout << right[1][0] << " " << right[1][1] << endl;
-        cout << "\t" << "    "; cout << back[0][0] << " " << back[0][1] << endl;
-        cout << "\t" << "    "; cout << back[1][0] << " " << back[1][1] << endl;
+        cout << "==============================" << endl;
+        cout << "\t" << "    "; cout << up[0][0] << " " << up[0][1] << endl;
+        cout << "\t" << "    "; cout << up[1][0] << " " << up[1][1] << endl;
+        cout << "       " << left[0][0]<< " " << left[0][1] << "  " << front[0][0]<<  " " << front[0][1] << "  " << right[0][0] << " " << right[0][1] << "  "; cout << back[0][0] << " " << back[0][1] << endl;
+        cout << "       " << left[1][0]<< " " << left[1][1] << "  " << front[1][0]<<  " " << front[1][1] << "  " << right[1][0] << " " << right[1][1] << "  "; cout << back[0][0] << " " << back[0][1] << endl;
+        cout << "\t" << "    "; cout << down[0][0] << " " << down[0][1] << endl;
+        cout << "\t" << "    "; cout << down[1][0] << " " << down[1][1] << endl;
         cout << endl;
+        cout<< "\t" << "MOVIMENTOS: " << endl;
+        cout<< "     " << "- U" << "   ";
+        cout << "- D" << "   ";
+        cout << "- L" << endl;
+        cout<< "     " << "- R" << "   ";
+        cout << "- F" << "   ";
+        cout << "- B" << "   " << endl;
     }
 };
 
@@ -214,8 +219,33 @@ class magicCubik2x2x2 {
 int main() {
     magicCubik2x2x2 cube;
     cout << "\n\t MAGIC CUBIK " << endl;
-    cout << "==============================" << endl;
     cube.printCubik();
+
+    char m;
+    while(true) {
+        cin >> m;
+        if (m == 'U') {
+            cube.moveU();
+            cube.printCubik();
+        } else if (m == 'D') {
+            cube.moveD();
+            cube.printCubik();
+        } else if (m == 'L') {
+            cube.moveL();
+            cube.printCubik();
+        } else if (m == 'R') {
+            cube.moveR();
+            cube.printCubik();
+        } else if (m == 'F') {
+            cube.moveF();
+            cube.printCubik();
+        } else if (m == 'B') {
+            cube.moveB();
+            cube.printCubik();
+        } else {
+            cout << "Instrução não reconhecida, tente novamente" << endl;
+        }
+    }
 
     return 0;
 }
