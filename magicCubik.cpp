201205851a208c0e56ed18bde380125f43131f41
RenatoHPissinatti <pissinatti.renato@outlook.com>
2025-08-26 19:53:59 -0300

    Inicialização na implementação gráfica do cubo

--- magicCubik.cpp
+++ magicCubik.cpp
@@ -4,9 +4,10 @@ using namespace std;
 #define ll long long
 #define fastio ios_base::sync_with_stdio(false);
 #define fo(i,n) for(i=0;i<n;i++)
-#define si(x)	cin >> x
+#define si(x)	cin >> x  
 #define pi(x)   cout << x
 #define pl(x)   cout << x << "\n"
+
 
 class magicCubik2x2x2 {
     private:
@@ -16,18 +17,39 @@ class magicCubik2x2x2 {
         string right[2][2];
         string front[2][2];
         string back[2][2];
+
+    string color (const string &s) const {
+        char c = s[0];
+        switch (c) {
+            case 'W': // White
+                return string("\033[47m  \033[0m"); break;
+            case 'Y': // Yellow
+                return string("\033[43m  \033[0m"); break;
+            case 'O': // Orange - usa 256-color (fallback: brownish)
+                return string("\033[48;5;208m  \033[0m"); break;
+            case 'R': // Red
+                return string("\033[41m  \033[0m"); break;
+            case 'G': // Green
+                return string("\033[42m  \033[0m"); break;
+            case 'B': // Blue
+                return string("\033[44m  \033[0m"); break;
+            default:
+                string out = s;
+                return out;
+        }
+    }
     
     public:
         // Inicializar o cubo:
         magicCubik2x2x2() {
             for (int i = 0; i < 2; ++i) {
                 for (int j = 0; j < 2; ++j) {
-                    up[i][j] = "W" + to_string(i) + to_string(j);
-                    down[i][j] = "Y" + to_string(i) + to_string(j);
-                    left[i][j] = "O" + to_string(i) + to_string(j);
-                    right[i][j] = "R" + to_string(i) + to_string(j);
-                    front[i][j] = "G" + to_string(i) + to_string(j);
-                    back[i][j] = "B" + to_string(i) + to_string(j);
+                    up[i][j] = "W" + to_string(i+j);
+                    down[i][j] = "Y" + to_string(i+j);
+                    left[i][j] = "O" + to_string(i+j);
+                    right[i][j] = "R" + to_string(i+j);
+                    front[i][j] = "G" + to_string(i+j);
+                    back[i][j] = "B" + to_string(i+j);
                 }
             }
         }
@@ -182,7 +204,7 @@ class magicCubik2x2x2 {
         right[0][1] = temp[0][0];
         right[1][1] = temp[0][1];
 
-        string tempBack = back[0][0]; //Errado
+        string tempBack = back[0][0];
         back[0][0] = back[0][1];
         back[0][1] = back[1][1];
         back[1][1] = back[1][0];
@@ -196,18 +218,31 @@ class magicCubik2x2x2 {
 
     void printCubik() {
         cout << "==============================" << endl;
-        cout << "\t" << "        "; cout << up[0][0] << " " << up[0][1] << endl;
-        cout << "\t" << "        "; cout << up[1][0] << " " << up[1][1] << endl;
-        cout << "       " << left[0][0]<< " " << left[0][1] << "  " << front[0][0]<<  " " << front[0][1] << "  " << right[0][0] << " " << right[0][1] << "  "; cout << back[0][0] << " " << back[0][1] << endl;
-        cout << "       " << left[1][0]<< " " << left[1][1] << "  " << front[1][0]<<  " " << front[1][1] << "  " << right[1][0] << " " << right[1][1] << "  "; cout << back[0][0] << " " << back[0][1] << endl;
-        cout << "\t" << "        "; cout << down[0][0] << " " << down[0][1] << endl;
-        cout << "\t" << "        "; cout << down[1][0] << " " << down[1][1] << endl;
+        // Up
+        cout << "\t" << color(up[0][0]) << color(up[0][1]) << endl;
+        cout << "\t" << color(up[1][0]) << color(up[1][1]) << endl;
+
+        // Middle: Left Front Right Back
+        cout << " " << color(left[0][0]) << color(left[0][1]) << "  "
+             << color(front[0][0]) << color(front[0][1]) << "  "
+             << color(right[0][0]) << color(right[0][1]) << "  "
+             << color(back[0][0]) << color(back[0][1]) << endl;
+
+        cout << " " << color(left[1][0]) << color(left[1][1]) << "  "
+             << color(front[1][0]) << color(front[1][1]) << "  "
+             << color(right[1][0]) << color(right[1][1]) << "  "
+             << color(back[1][0]) << color(back[1][1]) << endl;
+
+        // Down
+        cout << "\t" << color(down[0][0]) << color(down[0][1]) << endl;
+        cout << "\t" << color(down[1][0]) << color(down[1][1]) << endl;
         cout << endl;
-        cout<< "\t" << "MOVIMENTOS: " << endl;
-        cout<< "     " << "- U" << "   ";
+
+        cout<< "      " << "MOVIMENTOS: " << endl;
+        cout<< "   " << "- U" << "   ";
         cout << "- D" << "   ";
         cout << "- L" << endl;
-        cout<< "     " << "- R" << "   ";
+        cout<< "   " << "- R" << "   ";
         cout << "- F" << "   ";
         cout << "- B" << "   " << endl;
     }
