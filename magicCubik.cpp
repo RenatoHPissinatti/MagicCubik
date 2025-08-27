faca12198ce7197f13ec611f622e77bab6ba6a47
RaphaelBFagundes <raphael.bf1@puccampinas.edu.br>
2025-08-27 13:21:14 -0300

    Adição de armazenamento de estado e movimentos (opção 1)

--- magicCubik.cpp
+++ magicCubik.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
+#include <queue>
+#include <string>
+#include <iostream>
+
 using namespace std;
+
+struct CubikState {
+    string up[2][2];
+    string down[2][2];
+    string left[2][2];
+    string right[2][2];
+    string front[2][2];
+    string back[2][2];
+};
 
 class magicCubik2x2x2 {
 private:
@@ -9,6 +22,10 @@ private:
     string right[2][2];
     string front[2][2];
     string back[2][2];
+
+    std::queue<char> moveHistory; // Fila que armazena os movimentos
+    std::queue<CubikState> stateHistory; // Fila que armazena os estados
+
 
     // Retorna o "inner" colorido (2 espaços com background ANSI)
     string inner(const string &s) const {
@@ -52,6 +69,40 @@ public:
                 front[i][j] = "G" + to_string(i+j);
                 back[i][j] = "B" + to_string(i+j);
             }
+    }
+
+
+    void addMove(char m)
+    {
+        moveHistory.push(m);
+    }
+
+    std::queue<char> & getMoveHistory()
+    { 
+        return moveHistory; 
+    }
+
+    std::queue<CubikState> & getStateHistory()
+    { 
+        return stateHistory; 
+    }
+    
+
+    CubikState getCurrentState() const {
+        CubikState currentState;
+        for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                currentState.up[i][j] = up[i][j];
+                currentState.down[i][j] = down[i][j];
+                currentState.left[i][j] = left[i][j];
+                currentState.right[i][j] = right[i][j];
+                currentState.front[i][j] = front[i][j];
+                currentState.back[i][j] = back[i][j];
+            }
+        }
+        return currentState;
     }
 
     // Movimentos (mantidos sem alteração)
@@ -223,19 +274,52 @@ public:
 
 int main() {
     magicCubik2x2x2 cube;
-    cout << "\n\t MAGIC CUBIK " << endl;
+    std::cout << "\n\t MAGIC CUBIK " << std::endl;
     cube.printCubik();
+
+    cube.getMoveHistory().push('I');
+    cube.getStateHistory().push(cube.getCurrentState());
 
     char m;
     while (true) {
-        cin >> m;
-        if (m == 'U') { cube.moveU(); cube.printCubik(); }
-        else if (m == 'D') { cube.moveD(); cube.printCubik(); }
-        else if (m == 'L') { cube.moveL(); cube.printCubik(); }
-        else if (m == 'R') { cube.moveR(); cube.printCubik(); }
-        else if (m == 'F') { cube.moveF(); cube.printCubik(); }
-        else if (m == 'B') { cube.moveB(); cube.printCubik(); }
-        else { cout << "Instrução inválida, tente novamente" << endl; }
+        std::cout << "Digite um movimento (U, D, L, R, F, B)ou 'Q' para sair: ";
+        std::cin >> m;
+
+        if(m =='U' || m =='D' || m == 'L' || m == 'R' || m == 'F' || m == 'B') {
+            
+            switch(m)
+            {
+                case 'U':
+                    cube.moveU();
+                    break;
+                case 'D':
+                    cube.moveD();
+                    break;
+                case 'L':
+                    cube.moveL();
+                    break;
+                case 'R':
+                    cube.moveR();
+                    break;
+                case 'F':
+                    cube.moveF();
+                    break;
+                case 'B':
+                    cube.moveB();
+                    break;
+            }
+
+            cube.getMoveHistory.push(m);
+            cube.getStateHistory.push(cube.getCurrentState());
+
+            cube.printCubik();
+        } else if (m == 'Q' || m == 'q') {
+            break;
+        }
+        else
+        {
+            std::cout << "Movimento inválido. Tente novamente." << std::endl;
+        }
     }
     return 0;
 }
